a440fc6a7d28b15dfbb9a232f8d8f5106703cdc3
Armend Matiqi <mendimatiqi9@gmail.com>
2024-05-28 17:38:46 +0200

    skipTurn method

--- core/src/main/java/it/unibz/inf/pp/clash/model/impl/EventHandlerImpl.java
+++ core/src/main/java/it/unibz/inf/pp/clash/model/impl/EventHandlerImpl.java
@@ -78,7 +78,51 @@ public class EventHandlerImpl implements EventHandler {
 
 	@Override
 	public void skipTurn() {
-		// TODO Auto-generated method stub
+		public class EventHandler {
+			private int currentPlayerIndex;
+			private boolean ongoingMove;
+			private boolean hasReinforcementUnit;
+			private static final int FIRST_PLAYER = 0;
+			private static final int SECOND_PLAYER = 1;
+		
+			public EventHandler() {
+				this.currentPlayerIndex = FIRST_PLAYER;
+				this.ongoingMove = false;  // This should be set according to game logic
+				this.hasReinforcementUnit = true;  // This should be set according to game logic
+			}
+			public void skipTurn() {
+				// Check if the action is invalid
+				if (!hasReinforcementUnit || ongoingMove) {
+					System.out.println("Cannot skip turn: invalid action.");
+					return;
+				}
+		
+				// Switch to the next player
+				if (currentPlayerIndex == FIRST_PLAYER) {
+					currentPlayerIndex = SECOND_PLAYER;
+				} else {
+					currentPlayerIndex = FIRST_PLAYER;
+				}
+		
+				System.out.println("Turn skipped. Current player: " + getCurrentPlayer());
+			}
+		
+			// Getter for the current player
+			public int getCurrentPlayer() {
+				return currentPlayerIndex;
+			}
+		
+			// Setter for ongoingMove
+			public void setOngoingMove(boolean ongoingMove) {
+				this.ongoingMove = ongoingMove;
+			}
+		
+			// Setter for hasReinforcementUnit
+			public void setHasReinforcementUnit(boolean hasReinforcementUnit) {
+				this.hasReinforcementUnit = hasReinforcementUnit;
+			}
+		}
+		
 		
 	}
 
