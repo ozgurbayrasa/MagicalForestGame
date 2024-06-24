0fa2e13e59c578c3441cfb85531654704cdca223
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-06-24 03:08:13 +0200

    Add Hero Styles to Heros

--- core/src/main/java/it/unibz/inf/pp/clash/model/snapshot/Hero.java
+++ core/src/main/java/it/unibz/inf/pp/clash/model/snapshot/Hero.java
@@ -1,13 +1,19 @@
 package it.unibz.inf.pp.clash.model.snapshot;
 
+import it.unibz.inf.pp.clash.model.snapshot.impl.HeroImpl.HeroType;
+
 import java.io.Serializable;
 
 public interface Hero extends Serializable {
+
 
     int getHealth();
 
     void setHealth(int health);
 
     String getName();
+
+    HeroType getHeroType();
+
 
 }
--- core/src/main/java/it/unibz/inf/pp/clash/model/snapshot/impl/HeroImpl.java
+++ core/src/main/java/it/unibz/inf/pp/clash/model/snapshot/impl/HeroImpl.java
@@ -4,6 +4,7 @@ import it.unibz.inf.pp.clash.model.snapshot.Hero;
 
 import java.io.Serial;
 
+
 public class HeroImpl implements Hero {
 
     /**
@@ -12,6 +13,10 @@ public class HeroImpl implements Hero {
 	@Serial
     private static final long serialVersionUID = 1L;
 
+    public enum HeroType{
+        DEFFENSIVE,
+        OFFENSIVE
+    }
 	private int health;
 
     private final String name;
@@ -34,5 +39,15 @@ public class HeroImpl implements Hero {
     @Override
     public String getName() {
         return name;
+    }
+
+    @Override
+    public HeroType getHeroType() {
+        if(name.equalsIgnoreCase("Alice")){
+            return HeroType.DEFFENSIVE;
+        }
+        else{
+            return HeroType.OFFENSIVE;
+        }
     }
 }
