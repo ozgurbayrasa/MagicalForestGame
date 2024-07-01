59700fab7763bc9ad33ae6f7da7b09a010bb41aa
Armend Matiqi <mendimatiqi9@gmail.com>
2024-07-01 16:45:38 +0200

    trying to fix my problem

--- core/src/main/java/it/unibz/inf/pp/clash/model/exceptions/OccupiedTileException.java
+++ core/src/main/java/it/unibz/inf/pp/clash/model/exceptions/OccupiedTileException.java
@@ -4,6 +4,6 @@ import it.unibz.inf.pp.clash.model.snapshot.units.Unit;
 
 public class OccupiedTileException extends RuntimeException {
     public OccupiedTileException(Unit unit) {
-        super("There is already a unit on this tile:\n"+unit);
+        super("There is already a unit on this tile:\n" + unit);
     }
 }
